6d342a36438a08e3cf05bde17291699c0d9695b0
EasonBlack <eason820720@163.com>
2016-12-10 23:12:37 +0800

    df

--- re-project/HomeSys/client/app/viewSeries/series-row.js
+++ re-project/HomeSys/client/app/viewSeries/series-row.js
@@ -7,11 +7,9 @@ class SeriesRow extends React.Component {
 
     render() {
         const {headers, row} = this.props;
-        const _props = this.props._props;
         return <tr>
             {
                 headers.map(function (h) {
-                    console.log(typeof row[h]);
                     if(typeof row[h] == 'object' && row[h]) {
                         return <td>{row[h].join(',')}</td>
                     } else {
